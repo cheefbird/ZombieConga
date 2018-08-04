9f204ac2d8cf2c5b561fb0ba7564a1f775941098
Francis Breidenbach <fbreidenbach@gmail.com>
2018-08-04 00:34:13 -0700

    Set and add background image

--- ZombieConga/GameScene.swift
+++ ZombieConga/GameScene.swift
@@ -11,5 +11,9 @@ import SpriteKit
 class GameScene: SKScene {
   override func didMove(to view: SKView) {
     backgroundColor = SKColor.black
+    
+    let background = SKSpriteNode(imageNamed: "background1")
+    
+    addChild(background)
   }
 }
