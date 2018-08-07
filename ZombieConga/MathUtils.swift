c7a0526c04f6fd5a6174f161e8597372b37e0e3f
Francis Breidenbach <fbreidenbach@gmail.com>
2018-08-06 17:22:30 -0700

    Create MathUtils.swift

---
+++ ZombieConga/MathUtils.swift
@@ -0,0 +1,85 @@
+//
+//  MathUtils.swift
+//  ZombieConga
+//
+//  Created by Francis Breidenbach on 8/6/18.
+//  Copyright © 2018 Francis Breidenbach. All rights reserved.
+//
+
+import Foundation
+import CoreGraphics
+
+// MARK: - Operand Overrides
+
+func + (left: CGPoint, right: CGPoint) -> CGPoint {
+  return CGPoint(x: left.x + right.x, y: left.y + right.y)
+}
+
+func += (left: inout CGPoint, right: CGPoint) {
+  left = left + right
+}
+
+func - (left: CGPoint, right: CGPoint) -> CGPoint {
+  return CGPoint(x: left.x - right.x, y: left.y - right.y)
+}
+
+func -= (left: inout CGPoint, right: CGPoint) {
+  left = left - right
+}
+
+func * (left: CGPoint, right: CGPoint) -> CGPoint {
+  return CGPoint(x: left.x * right.x, y: left.y * right.y)
+}
+
+func *= (left: inout CGPoint, right: CGPoint) {
+  left = left * right
+}
+
+func * (point: CGPoint, scalar: CGFloat) -> CGPoint {
+  return CGPoint(x: point.x * scalar, y: point.y * scalar)
+}
+
+func *= (point: inout CGPoint, scalar: CGFloat) {
+  point = point * scalar
+}
+
+func / (left: CGPoint, right: CGPoint) -> CGPoint {
+  return CGPoint(x: left.x / right.x, y: left.y / right.y)
+}
+
+func /= ( left: inout CGPoint, right: CGPoint) {
+  left = left / right
+}
+
+func / (point: CGPoint, scalar: CGFloat) -> CGPoint {
+  return CGPoint(x: point.x / scalar, y: point.y / scalar)
+}
+
+func /= (point: inout CGPoint, scalar: CGFloat) {
+  point = point / scalar
+}
+
+// MARK: - CGPoint Extension
+
+extension CGPoint {
+  
+  func length() -> CGFloat {
+    return sqrt(x * x + y * y)
+  }
+  
+  func normalized() -> CGPoint {
+    return self / length()
+  }
+  
+  var angle: CGFloat {
+    return atan2(y, x)
+  }
+}
+
+
+
+
+
+
+
+
