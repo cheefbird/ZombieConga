48d0bcafb0ed0ca5cabde71257e5ae5d2c7577ca
Francis Breidenbach <fbreidenbach@gmail.com>
2018-08-06 17:00:33 -0700

    Forgotten checkins. Make zombie move towards click. Set bounds for universal app.

--- ZombieConga/GameScene.swift
+++ ZombieConga/GameScene.swift
@@ -9,13 +9,155 @@
 import SpriteKit
 
 class GameScene: SKScene {
+  
+  // MARK: - Properties
+  
+  let playableRect: CGRect
+  
+  let zombie: SKSpriteNode = SKSpriteNode(imageNamed: "zombie1")
+  
+  let zombieMovePointsPerSec: CGFloat = 480.0
+  var velocity = CGPoint.zero
+  
+  var lastUpdateTime: TimeInterval = 0
+  var dt: TimeInterval = 0
+  
+  // MARK: - Overrides
+  
+  override init(size: CGSize) {
+    let maxAspectRatio: CGFloat = 16.0 / 9.0
+    let playableHeight = size.width / maxAspectRatio
+    let playableMargin = (size.height - playableHeight) / 2.0
+    
+    playableRect = CGRect(x: 0, y: playableMargin, width: size.width, height: playableHeight)
+    
+    super.init(size: size)
+  }
+  
+  required init(coder aDecoder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
   override func didMove(to view: SKView) {
     backgroundColor = SKColor.black
     
     let background = SKSpriteNode(imageNamed: "background1")
     
     background.position = CGPoint(x: size.width / 2, y: size.height / 2)
+    background.zPosition = -1
+    
+    zombie.position = CGPoint(x: 400, y: 400)
     
     addChild(background)
+    addChild(zombie)
+    
+    debugDrawPLayableArea()
+  }
+  
+  override func update(_ currentTime: TimeInterval) {
+    // Show time elapsed since last update
+    if lastUpdateTime > 0 {
+      dt = currentTime - lastUpdateTime
+    } else {
+      dt = 0
+    }
+    
+    lastUpdateTime = currentTime
+    print("\(dt*1000) ms since last update")
+    
+    move(sprite: zombie, velocity: velocity)
+    
+    boundsCheckZombie()
+  }
+  
+  // MARK: - Touch Handling
+  
+  func sceneTouched(touchLocation: CGPoint) {
+    moveZombieToward(location: touchLocation)
+  }
+  
+  override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+    guard let touch = touches.first else {
+      return
+    }
+    
+    let touchLocation = touch.location(in: self)
+    sceneTouched(touchLocation: touchLocation)
+  }
+  
+  override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+    guard let touch = touches.first else {
+      return
+    }
+    
+    let touchLocation = touch.location(in: self)
+    sceneTouched(touchLocation: touchLocation)
+  }
+  
+  // MARK: - Methods
+  
+  func move(sprite: SKSpriteNode, velocity: CGPoint) {
+    let amountToMove = CGPoint(x: velocity.x * CGFloat(dt), y: velocity.y * CGFloat(dt))
+    
+    print("Amount to move: \(amountToMove)")
+    
+    sprite.position = CGPoint(x: sprite.position.x + amountToMove.x, y: sprite.position.y + amountToMove.y)
+  }
+  
+  func moveZombieToward(location: CGPoint) {
+    let offset = CGPoint(x: location.x - zombie.position.x, y: location.y - zombie.position.y)
+    
+    let length = sqrt(Double(offset.x * offset.x + offset.y * offset.y))
+    
+    let direction = CGPoint(x: offset.x / CGFloat(length), y: offset.y / CGFloat(length))
+    velocity = CGPoint(x: direction.x * zombieMovePointsPerSec, y: direction.y * zombieMovePointsPerSec)
+  }
+  
+  func boundsCheckZombie() {
+    let bottomLeft = CGPoint(x: 0, y: playableRect.minY)
+    let topRight = CGPoint(x: size.width, y: playableRect.maxY)
+    
+    if zombie.position.x <= bottomLeft.x {
+      zombie.position.x = bottomLeft.x
+      velocity.x = -velocity.x
+    }
+    if zombie.position.x >= topRight.x {
+      zombie.position.x = topRight.x
+      velocity.x = -velocity.x
+    }
+    if zombie.position.y <= bottomLeft.y {
+      zombie.position.y = bottomLeft.y
+      velocity.y = -velocity.y
+    }
+    if zombie.position.y >= topRight.y {
+      zombie.position.y = topRight.y
+      velocity.y = -velocity.y
+    }
+  }
+  
+  // MARK: - Debug Methods
+  
+  func debugDrawPLayableArea() {
+    let shape = SKShapeNode(rect: playableRect)
+    shape.strokeColor = SKColor.red
+    shape.lineWidth = 4.0
+    addChild(shape)
   }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
