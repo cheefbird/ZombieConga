352510e5edab758abd05fa3d02f8354e7440b574
Francis Breidenbach <fbreidenbach@gmail.com>
2018-08-07 18:56:11 -0700

    Implement randomNumber gen.

--- ZombieConga/MathUtils.swift
+++ ZombieConga/MathUtils.swift
@@ -95,6 +95,14 @@ func shortestAngleBetween(angle1: CGFloat,
 }
 
 extension CGFloat {
+  static func random() -> CGFloat {
+    return CGFloat(Float(arc4random()) / Float(UInt32.max))
+  }
+  
+  static func random(min: CGFloat, max: CGFloat) -> CGFloat {
+    assert(min < max)
+    return CGFloat.random() * (max - min) + min
+  }
   
   func sign() -> CGFloat {
     return self >= 0.0 ? 1.0 : -1.0
