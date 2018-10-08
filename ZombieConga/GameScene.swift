3e07e9f3a292deb55130e678e529803efeb50134
Francis Breidenbach <fbreidenbach@gmail.com>
2018-10-07 22:51:58 -0700

    Add lives label to bottom left

--- ZombieConga/GameScene.swift
+++ ZombieConga/GameScene.swift
@@ -37,6 +37,8 @@ class GameScene: SKScene {
   let cameraNode = SKCameraNode()
   let cameraMovePointsPerSec: CGFloat = 200.0
   
+  let livesLabel = SKLabelNode(fontNamed: "Chalkduster")
+  
   // MARK: - Optional Properties
   
   var lastTouchLocation: CGPoint?
@@ -115,6 +117,18 @@ class GameScene: SKScene {
     addChild(cameraNode)
     camera = cameraNode
     cameraNode.position = CGPoint(x: size.width / 2, y: size.height / 2)
+    
+    livesLabel.text = "Lives: X"
+    livesLabel.fontColor = SKColor.black
+    livesLabel.fontSize = 100
+    livesLabel.zPosition = 150
+    livesLabel.horizontalAlignmentMode = .left
+    livesLabel.verticalAlignmentMode = .bottom
+    livesLabel.position = CGPoint(
+      x: -playableRect.size.width/2 + CGFloat(20),
+      y: -playableRect.size.height/2 + CGFloat(20))
+    
+    cameraNode.addChild(livesLabel)
   }
   
   override func update(_ currentTime: TimeInterval) {
